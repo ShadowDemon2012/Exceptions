b86afd30a0bd8e76d1aee32a0c8130d944664fb8
Dmitriy <shadowgods@mail.ru>
2022-10-02 19:35:09 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static boolean acceptThreeParameters(String login,String password,String confirmPassword){
+    return false;
+    }
+}
